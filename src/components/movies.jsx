05239f63d505dfaac20e6368a8f33c150d74182f
Acoustic6 <6dorog@mail.ru>
2020-08-24 00:33:30 +0300

    Like component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getMovies, deleteMovie } from '../services/fakeMovieService';
+import Like from './common/like';
 
 class Movies extends Component {
   state = {
@@ -20,21 +21,25 @@ class Movies extends Component {
               <th scope="col">Stock</th>
               <th scope="col">Rate</th>
               <th scope="col"></th>
+              <th scope="col"></th>
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map(m =>
-              <tr key={m._id}>
-                <th>{m.title}</th>
-                <th>{m.genre.name}</th>
-                <th>{m.numberInStock}</th>
-                <th>{m.dailyRentalRate}</th>
-                <th>
-                  <button className="btn btn-danger" onClick={() => this.deleteMovieBy(m._id)}>Delete</button>
-                </th>
+            {this.state.movies.map(movie =>
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td><Like
+                  liked={movie.liked} movie={movie}
+                  onLikeStateChanged={() => this.handleLikeStateChanged(movie)} />
+                </td>
+                <td>
+                  <button className="btn btn-danger" onClick={() => this.deleteMovieBy(movie._id)}>Delete</button>
+                </td>
               </tr>
-            )
-            }
+            )}
           </tbody>
         </table>
       </div>
@@ -46,6 +51,14 @@ class Movies extends Component {
     this.setState({
       movies: getMovies()
     });
+  }
+
+  handleLikeStateChanged = movie => {
+    const movies = [...this.state.movies];
+    const index = this.state.movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
   }
 }
 
